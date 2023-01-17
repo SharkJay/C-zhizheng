3b7c61935b1888a3c6bc698470ec0e7baab715ba
jack <2149078244@qq.com>
2023-01-18 01:05:54 +0800

    第三次提交

--- 1-6指针/18指针数组.c
+++ 1-6指针/18指针数组.c
@@ -12,9 +12,9 @@ int main() {
 
 	//a是int类型，取地址后，&a是int*类型，所以数组num是int* [3]类型
 	int* num[] = { &a,&b,&c };
-	for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++) {
+	/*for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++) {
 		printf("%d\n", *num[i]);
-	}
+	}*/
 
 	//定义一个指针用来保存数组num首元素的地址
 	//num == &num[0] == &(int*)
@@ -24,7 +24,9 @@ int main() {
 	//通过k取第二个元素的值
 	printf("%d\n", **(k + 1));
 
-
+	for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++) {
+		printf("%d ", **(k + i));
+	}
 	system("pause");
 	return 0;
 }
